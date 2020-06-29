6697974fe7fbd08aa949cc78cf0956cfc31b6b36
michareichmann <micha.reichmann@gmail.com>
2020-06-29 12:16:17 +0200

    update and reformat constants and materials

--- utils/constants.h
+++ utils/constants.h
@@ -1,16 +1,14 @@
 #ifndef CONST_H
 #define CONST_H
 
-// Resolution analog planes in [um]
-#define resolution_analog 33e-3
+/** Resolution of the analogue planes in [mm] */
+#define r_cms_pix_x 150e-3 / sqrt(12)
+#define r_cms_pix_y 100e-3 / sqrt(12)
+#define resolution_analog 14e-3
+#define resolution_analog_y 9e-3
 
-// Resolution analog planes in [um]
-#define resolution_analog_y 51e-3
-
-// Resolution digital planes in [um]
+/** Resolution of the digital planes in [mm] */
 #define resolution_digital 33e-3
-
-// Resolution digital planes in [um]
 #define resolution_digital_y 51e-3
 
 #endif /* CONST_H */
--- utils/materials.h
+++ utils/materials.h
@@ -1,29 +1,15 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 
-// X0 Si = 21.82/2.33 = 9.365 cm
-#define X0_Si 93.65
-
-// X0 Diamond
+/** Radiation length [mm] */
+#define X0_Si 93.65       // 21.82/2.33 = 9.365 cm
 #define X0_Diamond 122.0
-  
-// X0 Al = 24.01/2.70 = 8.89 cm
-#define X0_Al 88.9
-
-// X0 Au = 0.3344 cm
+#define X0_Al 88.9        // 24.01/2.70 = 8.89 cm
 #define X0_Au 3.344
-
-// X0 Cu = 12.86/8.96 = 1.435 cm
-#define X0_Cu 14.35
-  
-// X0 air = 36.66/1.204E-3 = 303.9 m
-#define X0_Air 304200.0
-
-// X0 Kapton =  40.56 / 1.42 = 28.56 cm
-#define X0_Kapton  285.6
-
-// X0 PCB (FR4)
-// http://personalpages.to.infn.it/~tosello/EngMeet/ITSmat/SDD/SDD_G10FR4.html
-#define X0_PCB 167.608
+#define X0_Cu 14.35       // 12.86/8.96 = 1.435 cm
+#define X0_Air 304200.0   // 36.66/1.204E-3 = 303.9 m
+#define X0_Kapton  285.6  // 40.56 / 1.42 = 28.56 cm
+#define X0_PVC 196.3      // 25.51 / 1.30 = 19.63 cm
+#define X0_PCB 167.608    // FR4 http://personalpages.to.infn.it/~tosello/EngMeet/ITSmat/SDD/SDD_G10FR4.html
 
 #endif /* MATERIAL_H */
